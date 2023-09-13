61241604fde3ce361870b26aed791742acedfa9c
Alex Mcneilly <115044628+alex-mcneilly@users.noreply.github.com>
2023-09-13 10:58:42 -0400

    Create Toolbar.tsx

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Toolbar extends React.Component {
+    render() {
+        return (
+            <div className="toolbar">
+                {/* Toolbar with options like play, stop, save, etc. */}
+            </div>
+        );
+    }
+}
+
+export default Toolbar;
