52d760744c98ca427585cc47d987d9852e9ce267
Alex Mcneilly <115044628+alex-mcneilly@users.noreply.github.com>
2023-09-13 10:55:17 -0400

    Create App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Editor from './Editor';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <Editor />
+            </div>
+        );
+    }
+}
+
+export default App;
