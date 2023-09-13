b1f389e7afc786f8705c7c3c3a285fada76e3e4a
Alex Mcneilly <115044628+alex-mcneilly@users.noreply.github.com>
2023-09-13 10:58:14 -0400

    Create Blocks.tsx

---
+++ src/components/Blocks.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Blocks extends React.Component {
+    render() {
+        return (
+            <div className="blocks">
+                {/* Drag-and-drop blocks for coding */}
+            </div>
+        );
+    }
+}
+
+export default Blocks;
