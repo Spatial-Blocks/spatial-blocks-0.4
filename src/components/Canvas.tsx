18950902dad49287ee59b57a548b2fbe089386a7
Alex Mcneilly <115044628+alex-mcneilly@users.noreply.github.com>
2023-09-13 10:57:52 -0400

    Create Canvas.tsx

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Canvas extends React.Component {
+    render() {
+        return (
+            <div className="canvas">
+                {/* Canvas for Three.js rendering */}
+            </div>
+        );
+    }
+}
+
+export default Canvas;
