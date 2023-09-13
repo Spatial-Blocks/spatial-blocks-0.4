cb6921538eee86a8b516a7a086d26cda240f4760
Alex Mcneilly <115044628+alex-mcneilly@users.noreply.github.com>
2023-09-13 10:59:00 -0400

    Create Avatar.tsx

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Avatar extends React.Component {
+    render() {
+        return (
+            <div className="avatar">
+                {/* User's avatar or any related content */}
+            </div>
+        );
+    }
+}
+
+export default Avatar;
